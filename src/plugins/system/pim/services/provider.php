84156913acdf0e37bc4ba97f512ca6d9b5fb338b
Pieter-Jan de Vries <pieter@obix.nl>
2023-10-31 12:17:32 +0100

    Adds system plugin.

---
+++ src/plugins/system/pim/services/provider.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     WebwinkelKeur
+ *
+ * @author      Pieter-Jan de Vries/Obix webtechniek <pieter@obix.nl>
+ * @copyright   Copyright (C) 2023+ Obix webtechniek. All rights reserved.
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * @link        https://www.obix.nl
+ */
+
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use PIM\Plugin\System\Pim\Extension\Pim;
+
+return new class implements ServiceProviderInterface {
+    public function register(Container $container)
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $config  = (array)PluginHelper::getPlugin('system', 'wwk');
+                $subject = $container->get(DispatcherInterface::class);
+
+                /** @var \Joomla\CMS\Plugin\CMSPlugin $plugin */
+                /** @var \Joomla\CMS\Plugin\CMSPlugin $plugin */
+                $plugin = new Pim($subject, $config);
+                $plugin->setApplication(Factory::getApplication());
+
+                return $plugin;
+            }
+        );
+    }
+};
---
+++ src/plugins/system/pim/src/Extension/Pim.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @package    PIM System Plugin
+ *
+ * @author     Pieter-Jan de Vries/Obix webtechniek <pieter@obix.nl>
+ * @copyright  Copyright © 2023 Obix webtechniek. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       https://www.obix.nl
+ */
+
+namespace PIM\Plugin\System\Pim\Extension;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Database\DatabaseDriver;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
+
+/**
+ * Pim plugin.
+ *
+ * @package   PIM System Plugin
+ * @since     1.0.0
+ */
+class Pim extends CMSPlugin implements SubscriberInterface
+{
+    /**
+     * Application object
+     *
+     * @var    CMSApplication
+     * @since  1.0.0
+     */
+    protected $app;
+
+    /**
+     * Database object
+     *
+     * @var    DatabaseDriver
+     * @since  1.0.0
+     */
+    protected $db;
+
+    /**
+     * Affects constructor behavior. If true, language files will be loaded automatically.
+     *
+     * @var    boolean
+     * @since  1.0.0
+     */
+    protected $autoloadLanguage = true;
+
+    public function __construct(&$subject, $config = [])
+    {
+        parent::__construct($subject, $config);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        // TODO: Implement getSubscribedEvents() method.
+        return [
+            'onAfterRoute'        => 'handleAfterRoute'
+        ];
+    }
+
+    public function handleAfterRoute(Event $event)
+    {
+        $lang = $this->app->getLanguage();
+        $langTag = $lang->getTag();
+
+        if ($this->app->isClient('administrator'))
+        {
+            // administrator/language/en-GB/com_config.ini
+            $basePath = JPATH_ADMINISTRATOR;
+            $lang->load('com_config', $basePath, $langTag, true);
+        }
+    }
+
+}
